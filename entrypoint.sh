5de240c0fbbb2a44c19a2193f6efe83fd124f08b
David Waring <djw64@cornell.edu>
2022-04-11 13:37:17 -0400

    Add script to run tests to entrypoint use docker-compose.yml command to specify which tests to run

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,6 @@ sed -i s/localhost/$HOSTNAME/g /etc/slurm/slurm.conf
 # Fix file permissions
 echo "fixing file permissions..."
 tmp=$(cat "/home/production/cxgn/sgn/sgn_local.conf" | grep ^tempfiles_base | tr -s ' ' | xargs | cut -d ' ' -f 2)
-archive=$(cat "/home/production/cxgn/sgn/sgn_local.conf" | grep ^archive_path | tr -s ' ' | xargs | cut -d ' ' -f 2)
-submissions=$(cat "/home/production/cxgn/sgn/sgn_local.conf" | grep ^submission_path | tr -s ' ' | xargs | cut -d ' ' -f 2)
 static_content=$(cat "/home/production/cxgn/sgn/sgn_local.conf" | grep ^static_content_path | tr -s ' ' | xargs | cut -d ' ' -f 2)
 export="/home/production/export"
 export_prod="/home/production/export/prod"
@@ -19,9 +17,12 @@ tmp_run="/tmp/cxgn_tools_run"
 mkdir -p "$tmp"
 chown www-data:www-data "$tmp/../"
 mkdir -p "$tmp/mason/obj"; chown -R www-data:www-data "$tmp/mason"
-chown -R www-data:www-data "$archive"
-chown -R www-data:www-data "$submissions"
 mkdir -p "$static_content/folder"; chown -R www-data:www-data "$static_content/folder"
 mkdir -p "$export"; chown -R www-data:www-data "$export"; ln -snf "$export" /export
 mkdir -p "$export_prod"; chown -R www-data:www-data "$export_prod"
 mkdir -p "$tmp_run"; chown www-data:www-data "$tmp_run"
+
+# Run the tests
+mkdir -p logs
+echo "====> RUNNING TEST: ${@}"
+script -O logs/test.log -c "((perl t/test_fixture.pl \"$@\" | tee logs/test.out) 3>&1 1>&2 2>&3 | tee logs/test.err)"
